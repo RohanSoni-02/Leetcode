4abaa474ce9984e5cd09edd1f955181386df0a60
RohanSoni-02 <soni.rohanchd@gmail.com>
2025-05-19 23:28:35 +1000

    added valid palindrome

---
+++ validPalindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()){
+            return true;
+        }
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
+            }
+        }
+
+        int start=0;
+        int end = filtered.size()-1;
+        while(start<end){
+            if(filtered[start]!=filtered[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+};
